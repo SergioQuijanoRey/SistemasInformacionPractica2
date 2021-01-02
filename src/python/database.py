1affd04911b5999017f6ad205fab729ce5db998e
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-01-02 19:45:51 +0100

    feat: mejoro la estructura del manejo de bases de datos

--- src/python/database.py
+++ src/python/database.py
@@ -21,57 +21,24 @@ class DatabaseRepository:
     Por tanto, no puede ser instanciada directamente, debe ser implementada
     """
     def __init__(self):
-        raise Exception("DatabaseRepository es una interfaz, no puede ser instanciada")
+        """El inicializador se conecta a la base de datos"""
 
+        self.conn = None
+        self.cursor = None
 
-# Database selector
-database_selector = ["SQLite", "MariaDB"]
-database_selector = database_selector[1]
+        self.connect()
 
-if database_selector == "MariaDB":
-    try:
-        # Datos de la base de datos
-        server = '172.17.0.2'
-        database = 'base_datos_almacenamiento'
-        username = 'sergio'
-        password = 'sergioquijano'
-        port = 3306
+    def connect(self):
+        """Codigo que conecta con la base de datos"""
+        raise Exception("DatabaseRepository es una interfaz que no puede ser instanciada")
 
-        conn = mariadb.connect(
-            user=username,
-            password=password,
-            host=server,
-            port=port,
-            database=database
-        )
-
-    except Exception as err:
-        print("ERROR conectando con la base de datos de MariaDB")
-        print(f"El error fue {err}")
-        exit(-1)
-
-elif database_selector == "SQLite":
-    print("WARNING! Using testing database")
-    try:
-        conn = sqlite3.connect('example2.db')
-    except:
-        print("ERROR conectando con la base de datos de testing")
-        exit(-1)
-else:
-    print(
-        f"ERROR, el selector de base de datos '{database_selector}' no es un selector valido")
-    exit(-1)
-
-# En todas las bases de datos podemos tomar el cursor de la misma forma
-cursor = conn.cursor()
-
-def try_execute(query: str, err_msg: str = "ERROR! ejecutando una peticion a la base de datos", should_return : bool = False):
+    def try_execute(self, query: str, err_msg: str = "ERROR! ejecutando una peticion a la base de datos", should_return : bool = False):
     """Tries to execute a database query
     Shows error msg if it fails
     """
 
     try:
-        cursor.execute(query)
+        self.cursor.execute(query)
 
         if should_return == False:
             return "OK"
@@ -90,3 +57,41 @@ def try_execute(query: str, err_msg: str = "ERROR! ejecutando una peticion a la 
     except Exception as err:
         print(err_msg)
         print(f"El codigo del error fue: {err}")
+
+class SQLiteDatabase:
+    def connect(self):
+        try:
+            conn = sqlite3.connect('example2.db')
+        except:
+            print("ERROR conectando con la base de datos de testing")
+            exit(-1)
+
+        self.conn = conn
+        self.cursor = self.conn.cursor()
+
+class MariaDatabase:
+
+    def connect(self):
+        try:
+            # Datos de la base de datos
+            server = '172.17.0.2'
+            database = 'base_datos_almacenamiento'
+            username = 'sergio'
+            password = 'sergioquijano'
+            port = 3306
+
+            conn = mariadb.connect(
+                user=username,
+                password=password,
+                host=server,
+                port=port,
+                database=database
+            )
+
+        except Exception as err:
+            print("ERROR conectando con la base de datos de MariaDB")
+            print(f"El error fue {err}")
+            exit(-1)
+
+        self.conn = conn
+        self.cursor = self.conn.cursor()
