234bc93fdea60830a2dfddb745f89da986512732
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-01-03 13:56:54 +0100

    fix: soluciono bugs que hacian que el codigo no corriese

--- src/python/database.py
+++ src/python/database.py
@@ -4,10 +4,10 @@ import os
 
 class DatabaseFactory:
     """Clase que se encarga de devolver los tipos de bases de datos que soportamos"""
-    def get_database(database_type: str) -> DatabaseRepository:
+    def get_database(database_type: str):
         if database_type == "SQLite":
             return SQLiteDatabase()
-        elif database_type == "MariaDB"
+        elif database_type == "MariaDB":
             return MariaDatabase()
         else:
             print("ERROR! No hay una base de datos de ese tipo")
@@ -33,30 +33,32 @@ class DatabaseRepository:
         raise Exception("DatabaseRepository es una interfaz que no puede ser instanciada")
 
     def try_execute(self, query: str, err_msg: str = "ERROR! ejecutando una peticion a la base de datos", should_return : bool = False):
-    """Tries to execute a database query
-    Shows error msg if it fails
-    """
+        """
+        Tries to execute a database query
+        Shows error msg if it fails
+        """
 
-    try:
-        self.cursor.execute(query)
+        try:
+            self.cursor.execute(query)
 
-        if should_return == False:
-            return "OK"
+            if should_return == False:
+                return "OK"
 
-        # We have results to return
-        result = []
-        for row in cursor:
-            result.append(row)
+            # We have results to return
+            result = []
+            for row in cursor:
+                result.append(row)
 
-        # Safety check
-        if result == []:
-            raise Exception("No results when should_return is indicated as True")
+            # Safety check
+            if result == []:
+                raise Exception("No results when should_return is indicated as True")
 
-        return result
+            return result
 
-    except Exception as err:
-        print(err_msg)
-        print(f"El codigo del error fue: {err}")
+        except Exception as err:
+            print(err_msg)
+            print(f"El codigo del error fue: {err}")
+
 
 class SQLiteDatabase(DatabaseRepository):
     def connect(self):
--- src/python/main.py
+++ src/python/main.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
 
     # Tomamos el objeto tipo base de datos
     database_type = "SQLite"
-    database = database.DatabaseFactory.get_database(database_type)
-
+    db = database.DatabaseFactory.get_database(database_type)
