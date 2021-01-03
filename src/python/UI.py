5e428849e764ff1303b0724515e4c4d02636fcc9
Juan José Herrera Aranda <34545546+Kiedie@users.noreply.github.com>
2021-01-03 16:22:57 +0100

    Update UI.py

--- src/python/UI.py
+++ src/python/UI.py
@@ -5,7 +5,7 @@ def main_menu(db):
     print("Este es el menu principal")
     print("Hay que hacer cosas con el parametro db")
 
-def crear_actividad():
+def menu_actividad():
     idActividad = input('Inserte el identificador de Actividad:')
     while type(idActividad) != int:
         idActividad = input('Identificador no válido, inserte otro de nuevo:')
@@ -17,3 +17,49 @@ def crear_actividad():
     fecha = input('Inserte un fecha')
     #Como gestiono la correcta introducción de una fecha?
     return idActividad, descripcion, fecha
+
+def menu_asistente():
+    nombre = input("Introduce el nombre del asistente")
+    while type(nombre) != str or len(nomrbe)<= 0 or len(nombre) >300
+        nombre = input("Nombre no valido, insertelo de nuevo:")
+
+    dni = input("Introduce el dni del asistente")
+    while type(dni) != str or len(dni)<=0 or len(dni)>9
+        dni = input("Dni incorrecto, insertelo de nuevo:")
+    
+    cuentaBancaria = input("introduce el numero de cuenta bancaria")
+    while type(cuentaBancaria) != str or len(cuentaBancaria)>400 or len(cuentaBancaria)<= 0
+        cuentaBancaria = input("Cuenta Bancaria no valida, inserte otra")
+    
+    return nombre, dni, cuentaBancaria
+
+def menu_UsarEntrada():
+
+    idEntrada = input("Inserte el identificador de la entrada")
+    while type(idEntrada) != int
+        input("Identificador no valido, insertelo de nuevo:")
+
+    idActividad = input("Inserte el identificador de la actividad")
+    while type(idActividad) != int
+        input("Identificador no valido, insertelo de nuevo:")
+    
+    return idEntrada, idActividad
+
+def menu_AbonaPagos():
+    idPago = input("Introduce el identificador del pago")
+    while type(idPago) != int 
+        idPago =input("Identificador erroneo, insertelo de nuevo")
+    
+    cantidad = input("Introduce la cantidad del pago")
+    while type(cantidad) != float 
+        cantidad =input("cantidad erronea, insertela de nuevo")
+
+    idEntrada = input("Introduce el identificador de la entada")
+    while type(idEntrada) != int 
+        idEntrada =input("Identificador erroneo, insertelo de nuevo")
+
+    idActividad = input("Introduce el identificador de la Actividad")
+    while type(idActividad) != int 
+        idActividad =input("Identificador erroneo, insertelo de nuevo")
+
+    return idPago, cantidad, idEntrada, idActividad
