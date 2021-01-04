f65ce5090eadde0312906e85a5f2c33e81ba16c5
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-01-04 17:50:58 +0100

    feat: ahora la aplicacion tambien lanza el menu, despues de inicializar los datos

--- src/python/main.py
+++ src/python/main.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     database_type = "MariaDB"
     db = database.DatabaseFactory.get_database(database_type)
 
-    # # Lanzamos la aplicacion
-    # UI.main_menu(db)
+    # Lanzamos la aplicacion
+    UI.main_menu(db)
 
     # Finaliza la aplicacion
     print("Aplicacion finalizada con exito")
