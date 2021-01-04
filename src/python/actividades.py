6fd33b8996b745e0066a0c1b8abb3e2ad5491214
luciasalamanca <luciasalamancalopez@gmail.com>
2021-01-04 19:17:07 +0100

    Primera version del hora invitado haciendo el commit como el putito sergio quiere

--- src/python/actividades.py
+++ src/python/actividades.py
@@ -1,5 +1,6 @@
 import database
 import UI 
+from utils import get_usr_data
 
 def crear_actividad(db):
 
@@ -13,7 +14,15 @@ def rueda_pelicula(db):
     pass
 
 def hora_invitado(db):
-    pass
+    dni = input("Inserte el dni del Invitado (ej: 00000000X): ")
+    alfombra = get_usr_data("Inserte el identificador de la alfombra roja: ", int, "El dato introducido no es un entero")
+    hora = input("Inserte la hora a la que acude el invitado ( ej: 20:00:00): ")
+
+    db.try_execute(
+        f"INSERT INTO Acudir(DNIInvitado, idAlfombraRoja, hora) VALUES (\"{dni}\", {alfombra}, \"{hora}\")"
+    )
+
+    
 
 def permiso_periodista(db):
     pass
