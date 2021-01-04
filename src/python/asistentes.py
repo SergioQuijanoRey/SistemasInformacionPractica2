e30ef298e47cdc24533dddcec1a21d7fe028c519
Juan José Herrera Aranda <34545546+Kiedie@users.noreply.github.com>
2021-01-04 12:32:23 +0100

    Update asistentes.py
    añado comprar_entrada

--- src/python/asistentes.py
+++ src/python/asistentes.py
@@ -2,7 +2,29 @@ import database
 import UI
 
 def comprar_entrada(db):
-    pass
+    #Tomamos los datos
+    nombre, dni, cuentaBancaria = UI.input_asistente()
+    idEntrada, idActividad, dniAsistente, cantidadEntradas = UI.input_UsarEntrada()
+    idPago, cantidadPago, idEntrada2, idActividad2 = UI.input_AbonaPagos()
+    
+    #Garantizamos consistencia
+    while idEntrada2 != idEntrada or idActividad != idActividad2:
+        print("Error en la introduccion de la tupla AbonaPagos, inserte de nuevo")
+        idPago, cantidad, idEntrada2, idActividad2 = UI.input_AbonaPagos()
+
+    #Insercción garantizando la transacción
+    db.savepoint("ComprarEntrada") 
+
+    db.try_execute(
+        f"INSERT INTO UsarEntradas VALUES ({IdEntrada}, {idActividad}, {dniAsistente}, {cantidadEntradas})" )#La devolucion se gestiona en el disparador 
+
+    db.try_execute(
+        f"INSERT INTO AbonaPagos VALUES ({idPago},{cantidadPago},{idEntrada}, {idActividad})")
+
+    db.rollback("ComprarEntrada")
+
+    #Hacer efectivos los cambios 
+    db.commit()
 
 def covid(db):
     pass
