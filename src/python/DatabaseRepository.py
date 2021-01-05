ac44e80cb80874d8b228b4b61132744b8b3375e9
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-01-05 20:05:18 +0100

    feat: creo nuevas formas de lanzar peticiones de SQL

--- src/python/DatabaseRepository.py
+++ src/python/DatabaseRepository.py
@@ -3,11 +3,13 @@ import mariadb
 import secrets
 import os
 
+
 class DatabaseRepository:
     """
     Interfaz que declara las operaciones que puede realizarse con una base de datos
     Por tanto, no puede ser instanciada directamente, debe ser implementada
     """
+
     def __init__(self):
         """El inicializador se conecta a la base de datos"""
 
@@ -25,16 +27,46 @@ class DatabaseRepository:
 
     def connect(self):
         """Codigo que conecta con la base de datos"""
-        raise Exception("DatabaseRepository es una interfaz que no puede ser instanciada")
+        raise Exception(
+            "DatabaseRepository es una interfaz que no puede ser instanciada")
 
     def initialize_data(self):
         """Carga los datos iniciales en la base de datos a traves de ficheros sql"""
-        raise Exception("DatabaseRepository es una interfaz que no puede ser instanciada")
+        raise Exception(
+            "DatabaseRepository es una interfaz que no puede ser instanciada")
+
+    def execute(self, query: str):
+        """
+        Lanza una peticion a la base de datos
+        Si la base de datos lanza una excepcion, es responsabilidad del invocante manejar las excepciones
+        """
+
+        # Las sentencias con SELECT deben retornar
+        should_return = False
+        if "SELECT" in query or "select" in query:
+            should_return = True
+
+        self.cursor.execute(query)
+
+        if should_return == False:
+            return "OK"
+
+        # We have results to return
+        result = []
+        for row in self.cursor:
+            result.append(row)
+
+        # Safety check
+        if result == []:
+            raise Exception(
+                "No results when should_return is indicated as True")
+
+        return result
 
     def try_execute(self, query: str, err_msg: str = "ERROR! ejecutando una peticion a la base de datos", ignore_error: bool = False):
         """
-        Tries to execute a database query
-        Shows error msg if it fails
+        Intenta ejecutar una sentencia
+        Si la base de datos lanza una excepcion, la funcion la captura y muestra el mensaje de error
         """
 
         # Las sentencias con SELECT deben retornar
@@ -43,21 +75,12 @@ class DatabaseRepository:
             should_return = True
 
         try:
-            self.cursor.execute(query)
+            results = self.execute(query)
 
             if should_return == False:
                 return "OK"
 
-            # We have results to return
-            result = []
-            for row in cursor:
-                result.append(row)
-
-            # Safety check
-            if result == []:
-                raise Exception("No results when should_return is indicated as True")
-
-            return result
+            return results
 
         except Exception as err:
             if ignore_error == False:
@@ -79,13 +102,16 @@ class DatabaseRepository:
         sqlSentences = [sentence + ";" for sentence in sqlSentences]
 
         for sentence in sqlSentences:
-            self.try_execute(sentence, f"ERROR! Ejecutando sentencia {sentence}", ignore_error = ignore_error)
+            self.try_execute(
+                sentence, f"ERROR! Ejecutando sentencia {sentence}", ignore_error=ignore_error)
 
     def rollback(self, savepoint: str):
-        self.try_execute(f"ROLLBACK TO {savepoint};", f"ERROR haciendo el Rollback al savepoint {savepoint}")
+        self.try_execute(
+            f"ROLLBACK TO {savepoint};", f"ERROR haciendo el Rollback al savepoint {savepoint}")
 
     def savepoint(self, name: str):
-        self.try_execute(f"SAVEPOINT {name};", f"Trying to set savepoint {name} failed")
+        self.try_execute(f"SAVEPOINT {name};",
+                         f"Trying to set savepoint {name} failed")
 
     def commit(self):
         """Saves  the current changes to the database"""
@@ -98,7 +124,9 @@ class DatabaseRepository:
 
     def load_triggers(self):
         """Carga los triggers de la base de datos, a partir de una serie de ficheros"""
-        raise Exception("DatabaseRepository es una interfaz que no puede ser instanciada")
+        raise Exception(
+            "DatabaseRepository es una interfaz que no puede ser instanciada")
+
 
 class MariaDatabase(DatabaseRepository):
 
@@ -128,7 +156,8 @@ class MariaDatabase(DatabaseRepository):
         self.cursor = self.conn.cursor()
 
     def initialize_data(self):
-        self.try_execute_sql_file("./src/sql/CreacionTablas.sql", ignore_error = True)
+        self.try_execute_sql_file(
+            "./src/sql/CreacionTablas.sql", ignore_error=True)
 
     def load_triggers(self):
         triggers = [
@@ -137,6 +166,7 @@ class MariaDatabase(DatabaseRepository):
 
         for trigger in triggers:
             self.try_execute_sql_file(trigger)
+
 
 class SQLiteDatabase(DatabaseRepository):
     def connect(self):
@@ -159,5 +189,3 @@ class SQLiteDatabase(DatabaseRepository):
 
         for trigger in triggers:
             self.try_execute_sql_file(trigger)
-
-
