fed31286107c1b658c1821eb7aae81ad09bfc039
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-01-05 20:16:58 +0100

    fix: limpio duplicidades

--- src/python/DatabaseRepository.py
+++ src/python/DatabaseRepository.py
@@ -167,25 +167,3 @@ class MariaDatabase(DatabaseRepository):
         for trigger in triggers:
             self.try_execute_sql_file(trigger)
 
-
-class SQLiteDatabase(DatabaseRepository):
-    def connect(self):
-        try:
-            conn = sqlite3.connect('example.db')
-        except:
-            print("ERROR conectando con la base de datos de testing")
-            exit(-1)
-
-        self.conn = conn
-        self.cursor = self.conn.cursor()
-
-    def initialize_data(self):
-        self.try_execute_sql_file("./src/sql/CreacionTablas.sql")
-
-    def load_triggers(self):
-        triggers = [
-            "./src/sql/Triggers.sql"
-        ]
-
-        for trigger in triggers:
-            self.try_execute_sql_file(trigger)
