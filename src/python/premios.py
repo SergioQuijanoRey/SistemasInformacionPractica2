d7f5eda425cf69e172775b43ac6aebfd08fe0622
Juanjo <jh16357@gmail.com>
2021-01-06 21:47:08 +0100

    añado la funcino de votar

--- src/python/premios.py
+++ src/python/premios.py
@@ -2,13 +2,21 @@ import DatabaseRepository
 import UI
 
 def votar(db):
-    pass
+    db.mostrar_nominados()
+    db.mostrar_jueces()
+    dniJuez, idCategoria, dniNominado = UI.input_votacion()
+
+    db.try_execute(
+        f"INSERT INTO VotarNominado(DNIJuez, IdCategoria, DNINominado) VALUES (\"{dniJuez}\", {idCategoria},\"{dniNominado}\")"
+    )
+
+    db.commit()
 
 def planificar_premio(db):
     pass
 
 def asignar_nominado(db):
-    
+
     db.mostrar_nominados()
     dni, idcategoria = UI.input_candidato()
 
