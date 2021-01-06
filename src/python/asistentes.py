343e9855b5673a9727433b8b8969e5b18c85f3b8
Juanjo <jh16357@gmail.com>
2021-01-06 16:15:02 +0100

    Añado parte de la funcino comprar_entrada, aun esta incompleta pero funciona lo que lleva

--- src/python/asistentes.py
+++ src/python/asistentes.py
@@ -5,24 +5,22 @@ from utils import get_usr_data
 def comprar_entrada(db):
     #Tomamos los datos
     nombre, dni, cuentaBancaria = UI.input_asistente()
-    idEntrada, idActividad, dniAsistente, cantidadEntradas = UI.input_UsarEntrada()
-    idPago, cantidadPago, idEntrada2, idActividad2 = UI.input_AbonaPagos()
+    idEntrada, idActividad, cantidadEntradas = UI.input_UsarEntrada()
+    cantidadPago = UI.input_AbonaPagos()
 
-    #Garantizamos consistencia
-    while idEntrada2 != idEntrada or idActividad != idActividad2:
-        print("Error en la introduccion de la tupla AbonaPagos, inserte de nuevo")
-        idPago, cantidad, idEntrada2, idActividad2 = UI.input_AbonaPagos()
-
-    #Insercción garantizando la transacción
-    db.savepoint("ComprarEntrada")
+    #db.savepoint("ComprarEntrada")
 
     db.try_execute(
-        f"INSERT INTO UsarEntradas VALUES ({IdEntrada}, {idActividad}, {dniAsistente}, {cantidadEntradas})" )#La devolucion se gestiona en el disparador
+        f"INSERT INTO Asistentes(DNIAsistente, Nombre, CuentaBancaria) VALUES (\"{dni}\",\"{nombre}\",\"{cuentaBancaria}\")"
+    )
 
     db.try_execute(
-        f"INSERT INTO AbonaPagos VALUES ({idPago},{cantidadPago},{idEntrada}, {idActividad})")
+        f"INSERT INTO UsarEntradas(IdEntrada, IdActividad, DNIAsistentes, Cantidad) VALUES ({idEntrada}, {idActividad}, \"{dni}\", {cantidadEntradas})" )#La devolucion se gestiona en el disparador
 
-    db.rollback("ComprarEntrada")
+    db.try_execute(
+        f"INSERT INTO AbonaPagos(Cantidad, IdEntrada, IdActividad) VALUES ({cantidadPago},{idEntrada}, {idActividad})")
+
+    #db.rollback("ComprarEntrada")
 
     #Hacer efectivos los cambios
     db.commit()
